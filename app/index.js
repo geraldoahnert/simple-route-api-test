f59d5d0495e097c84234e540141dedbbd65cb97e
Geraldo Ahnert <geraldoahnert@gmail.com>
2021-10-05 19:32:12 -0300

    feat: simple express server

--- app/index.js
+++ app/index.js
@@ -1,11 +1,5 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const app = require('./server');
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello World!' });
-});
-
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log('Listening at http://localhost:3000');
 });
