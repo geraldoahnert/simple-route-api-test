295f9b1f77158bb99cb07a0cf2674d523d54bb5e
Geraldo Ahnert <geraldoahnert@gmail.com>
2021-10-05 19:32:27 -0300

    feat: simple test with supertest

--- app/index.spec.js
+++ app/index.spec.js
@@ -1,5 +1,11 @@
-describe('Sample test', () => {
-  it('should test that true === true', () => {
-    expect(true).toBe(true);
+const request = require('supertest');
+const app = require('./server');
+
+describe('Test my app server', () => {
+  it('should get main route', async () => {
+    const res = await request(app).get('/');
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('message');
   });
 });
